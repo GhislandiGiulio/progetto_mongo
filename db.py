1a37baecb7b905200c147c91f243e2a852844cba
GhislandiGiulio <giulio.ghislandi@itsrizzoli.it>
2024-06-29 13:31:27 +0200

    aggiunta funzione per inserire nuovo utente

--- db.py
+++ db.py
@@ -1,3 +1,21 @@
+from pymongo import MongoClient
+
 # definizione costanti
 uri = "mongodb://localhost:27017"
 nome_db = "db_concerti"
+
+# connessione al database MongoDB
+client = MongoClient(uri)
+db = client[nome_db]
+
+def inserisci_nuovo_utente(utente: dict):
+    """
+    Funzione che inserisce un utente nel database
+    """
+
+    global client, db
+
+    # selezione della collezione
+    coll = db["users"]
+
+    coll.insert_one(utente)
